8341dc280134b7626f03ea6c986a5878b01f4925
Ranjan Kumar <ranjankumar188@gmail.com>
2020-01-23 10:14:54 +0530

    Updated

--- list_response_time.rb
+++ list_response_time.rb
@@ -7,9 +7,10 @@ if ARGV.empty?
 else
   INPUT_TEXT = ARGV[0]
 end
+LOG_FILE='var/log/foreman/production.log'
 
 matched_lines = []
-File.foreach("var/log/foreman/production.log") do |line|
+File.foreach(LOG_FILE) do |line|
   text_match = line.match(INPUT_TEXT)
   next unless text_match
   matched_lines << line
@@ -19,7 +20,7 @@ RESPONSE_TEXT = "Completed 200 OK in"
 
 matched_lines.each do |line|
   id = line[/\[(.*?)\]/m, 1]
-  File.foreach("var/log/foreman/production.log") do |log|
+  File.foreach(LOG_FILE) do |log|
     if log.include?(RESPONSE_TEXT) && log.include?(id)
       print line
       print log
